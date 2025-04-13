00f6518c69ea0e7e274818ad42c7f6079d1979e7
dongwk <dwk126@126.com>
2025-04-13 11:57:42 +0800

    完善死信队列的描述，已知的结论死信队列只能手工处理，有本次提交的第二张图的说明，是阿里云的消息队列产品的描述作为参考

--- InternalOrderTest.java
+++ InternalOrderTest.java
@@ -68,20 +68,22 @@ public class InternalOrderTest {
                 .setTopics(topic)
                 .setClientConfiguration(configuration)
                 .build();
-        // 普通消息发送。
-        Message message = provider.newMessageBuilder()
-                .setTopic(topic)
-                // 设置消息索引键，可根据关键字精确查找某条消息。
-                .setKeys("messageKey")
-                // 设置消息Tag，用于消费端根据指定Tag过滤消息。
-                .setTag("messageTag")
-                // 消息体。
-                .setBody("messageBody".getBytes())
-                .build();
         try {
-            // 发送消息，需要关注发送结果，并捕获失败等异常。
-            SendReceipt sendReceipt = producer.send(message);
-            log.info("Send message successfully, messageId={}", sendReceipt.getMessageId());
+            for (int i = 0; i < 1; i++) {
+                // 普通消息发送。
+                Message message = provider.newMessageBuilder()
+                        .setTopic(topic)
+                        // 设置消息索引键，可根据关键字精确查找某条消息。
+                        .setKeys("messageKey")
+                        // 设置消息Tag，用于消费端根据指定Tag过滤消息。
+                        .setTag("messageTag")
+                        // 消息体。
+                        .setBody(("及时消息 messageBody "+(i+1)+" time:"+ DateUtils.getTime()).getBytes())
+                        .build();
+                // 发送消息，需要关注发送结果，并捕获失败等异常。
+                SendReceipt sendReceipt = producer.send(message);
+                log.info("Send message successfully, num={}, messageId={}", (i+1), sendReceipt.getMessageId());
+            }
         } catch (ClientException e) {
             log.error("Failed to send message", e);
         }
@@ -101,15 +103,27 @@ public class InternalOrderTest {
                 .setEndpoints(endpoints)
                 .build();
         // 订阅消息的过滤规则，表示订阅所有Tag的消息。
-        String tag = "*";
+        String tag = "messageTag";
         FilterExpression filterExpression = new FilterExpression(tag, FilterExpressionType.TAG);
         // 为消费者指定所属的消费者分组，Group需要提前创建。
-        String consumerGroup = "FIFOGroup";
+            String consumerGroup = "YourConsumerGroup"; // 普通消费组
+//        String consumerGroup = "FIFOGroup"; // 顺序消费组
+//        String consumerGroup = "rocketmq_demo"; // 普通消费组
 
         // 指定需要订阅哪个目标Topic，Topic需要提前创建。
-//        String topic = "TopicTest"; // 普通消息
+        String topic = "TopicTest"; // 普通消息
 //        String topic = "TopicDelay"; // 延时消息
-        String topic = "TopicFifo"; // 顺序消息
+//        String topic = "TopicFifo"; // 顺序消息
+//        String topic = "%DLQ%YourConsumerGroup"; // 死信消息
+        // 关于死信消息的几点：
+        // 1、死信消息，手动重发，手动重发后死信消息也不会删除，有效期后会自动删除，但是我这边验的都已经很久之前的了还不会删除，待定吧
+        // 2、死信队列的处理，虽然叫死信队列，单是应该是死信消息 https://help.aliyun.com/zh/apsaramq-for-rocketmq/cloud-message-queue-rocketmq-4-x-series/user-guide/dead-letter-queues
+        //
+        // 3、我自己测的死信消息重发，这个是4月8号发的私信消息，今天是4月13号了都没删除，不知道是不是默认有效期的问题，有效期是有默认配置的3天，没时间去看这个默认配置了
+        // 11:32:48.375 [RocketmqMessageConsumption-0-13] INFO internal.InternalOrderTest -- Consume message successfully, messageId=010A002700000B7B380806A69700000000, messageBody=及时消息 messageBody 1 time:2025-04-08 19:46:31
+        //===================
+        // 11:32:52.235 [RocketmqMessageConsumption-0-14] INFO internal.InternalOrderTest -- Consume message successfully, messageId=010A002700000B92940807EDC400000000, messageBody=及时消息 messageBody 1 time:2025-04-09 19:02:28
+        // 4、死信消息重试，我不管订阅死信的topic还是原本的topic都能消费，不知道因为什么
         // 初始化PushConsumer，需要绑定消费者分组ConsumerGroup、通信参数以及订阅关系。
         PushConsumer pushConsumer = provider.newPushConsumerBuilder()
                 .setClientConfiguration(clientConfiguration)
@@ -119,12 +133,23 @@ public class InternalOrderTest {
                 .setSubscriptionExpressions(Collections.singletonMap(topic, filterExpression))
                 // 设置消费监听器。
                 .setMessageListener(messageView -> {
+                    System.out.println("===================");
+//                    log.info("Consume start messageId={}", messageView.getMessageId());
+//                    if (messageView.getMessageId() != null) {
+////                        try {
+////                            Thread.sleep(1000000);
+////                        } catch (InterruptedException e) {
+////                            throw new RuntimeException(e);
+////                        }
+//                        throw new RuntimeException("测试消费");
+//                    }
+
                     // 处理消息并返回消费结果。
                     log.info("Consume message successfully, messageId={}, messageBody={}", messageView.getMessageId(), StandardCharsets.UTF_8.decode(messageView.getBody()));
                     return ConsumeResult.SUCCESS;
                 })
                 .build();
-        Thread.sleep(Long.MAX_VALUE);
+        Thread.sleep(5000000);
         // 如果不需要再使用 PushConsumer，可关闭该实例。
         // pushConsumer.close();
     }
@@ -214,87 +239,41 @@ public class InternalOrderTest {
      */
     @Test
     public void receiveMqSample() throws Exception {
-        final ClientServiceProvider provider = ClientServiceProvider.loadService();
+        // 接入点地址，需要设置成Proxy的地址和端口列表，一般是xxx:8080;xxx:8081。
+        String endpoint = "10.10.23.139:8080";
 
-        // Credential provider is optional for client configuration.
-        String accessKey = "yourAccessKey";
-        String secretKey = "yourSecretKey";
-        SessionCredentialsProvider sessionCredentialsProvider =
-                new StaticSessionCredentialsProvider(accessKey, secretKey);
-
-        String endpoints = pNamesrvAddr;
-        ClientConfiguration clientConfiguration = ClientConfiguration.newBuilder()
-                .setEndpoints(endpoints)
-//                .setCredentialProvider(sessionCredentialsProvider)
-                .build();
-        String consumerGroup = groupName;
-        Duration awaitDuration = Duration.ofSeconds(30);
-        String tag = "TagA";
+        // 消费示例：使用 SimpleConsumer 消费普通消息，主动获取消息处理并提交。
+        ClientServiceProvider provider = ClientServiceProvider.loadService();
         String topic = "TopicTest";
-        FilterExpression filterExpression = new FilterExpression(tag, FilterExpressionType.TAG);
-        // In most case, you don't need to create too many consumers, singleton pattern is recommended.
-        SimpleConsumer consumer = provider.newSimpleConsumerBuilder()
-                .setClientConfiguration(clientConfiguration)
-                // Set the consumer group name.
-                .setConsumerGroup(consumerGroup)
-                // set await duration for long-polling.
-                .setAwaitDuration(awaitDuration)
-                // Set the subscription for the consumer.
+        FilterExpression filterExpression = new FilterExpression("messageTag", FilterExpressionType.TAG);
+        SimpleConsumer simpleConsumer = provider.newSimpleConsumerBuilder()
+                // 设置消费者分组。
+                .setConsumerGroup("YourConsumerGroup")
+                // 设置接入点。
+                .setClientConfiguration(ClientConfiguration.newBuilder().setEndpoints(endpoint).build())
+                // 设置预绑定的订阅关系。
                 .setSubscriptionExpressions(Collections.singletonMap(topic, filterExpression))
-                .build();
-        // Max message num for each long polling.
-        int maxMessageNum = 16;
-        // Set message invisible duration after it is received.
-        Duration invisibleDuration = Duration.ofSeconds(15);
-        // Receive message, multi-threading is more recommended.
-        do {
-            final List<MessageView> messages = consumer.receive(maxMessageNum, invisibleDuration);
-            log.info("Received {} message(s)", messages.size());
-            for (MessageView message : messages) {
-                final MessageId messageId = message.getMessageId();
-                try {
-                    consumer.ack(message);
-                    log.info("Message is acknowledged successfully, messageId={}", messageId);
-                } catch (Throwable t) {
-                    log.error("Message is failed to be acknowledged, messageId={}", messageId, t);
-                }
-            }
-        } while (true);
-        // Close the simple consumer when you don't need it anymore.
-        // You could close it manually or add this into the JVM shutdown hook.
-        // consumer.close();
-    }
-
-    /**
-     * 延时消息，自定义时间.
-     */
-    @Test
-    public void sendDeayMqWithSelfDefTime() throws Exception {
-
-        final ClientServiceProvider provider = ClientServiceProvider.loadService();
-
-        String topic = "yourDelayTopic";
-        final Producer producer = ProducerSingleton.getInstance(topic);
-        // Define your message body.
-        byte[] body = "This is a delay message for Apache RocketMQ".getBytes(StandardCharsets.UTF_8);
-        String tag = "yourMessageTagA";
-        Duration messageDelayTime = Duration.ofSeconds(10);
-        final org.apache.rocketmq.client.apis.message.Message message = provider.newMessageBuilder()
-                // Set topic for the current message.
-                .setTopic(topic)
-                // Message secondary classifier of message besides topic.
-                .setTag(tag)
-                // Key(s) of the message, another way to mark message besides message id.
-                .setKeys("yourMessageKey-3ee439f945d7")
-                // Set expected delivery timestamp of message.
-                .setDeliveryTimestamp(System.currentTimeMillis() + messageDelayTime.toMillis())
-                .setBody(body)
+                // 设置从服务端接受消息的最大等待时间
+                .setAwaitDuration(Duration.ofSeconds(1))
                 .build();
         try {
-            final SendReceipt sendReceipt = producer.send(message);
-            log.info("Send message successfully, messageId={}", sendReceipt.getMessageId());
-        } catch (Throwable t) {
-            log.error("Failed to send message", t);
+            // SimpleConsumer 需要主动获取消息，并处理。
+            List<MessageView> messageViewList = simpleConsumer.receive(10, Duration.ofSeconds(30));
+            messageViewList.forEach(messageView -> {
+                System.out.println(messageView);
+                log.info(StandardCharsets.UTF_8.decode(messageView.getBody()).toString());
+                // 消费处理完成后，需要主动调用 ACK 提交消费结果。
+                try {
+                    simpleConsumer.ack(messageView);
+                } catch (ClientException e) {
+                    log.error("Failed to ack message, messageId={}", messageView.getMessageId(), e);
+                }
+            });
+        } catch (ClientException e) {
+            // 如果遇到系统流控等原因造成拉取失败，需要重新发起获取消息请求。
+            log.error("Failed to receive message", e);
         }
+        Thread.sleep(5000);
     }
+
 }
